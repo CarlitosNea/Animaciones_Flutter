3926f7d820eeba775994c83b38a14aa13ed2df4d
CarlitosNea <cardarober@gmail.com>
2023-04-27 10:40:29 -0500

    creacion fonts

--- lib/pantallas/pantalla2.dart
+++ lib/pantallas/pantalla2.dart
@@ -6,8 +6,58 @@ class PantallaDos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightBlue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hola mundo"),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: <Color>[
+                  Colors.black,
+                  Colors.lightBlueAccent
+                ],
+                begin: Alignment.topCenter
+            )
+        ),
+        width: double.infinity,
+        height: double.infinity,
+        child: ListView(
+          children: [
+            ListView.builder(
+                itemCount: 10,
+              shrinkWrap: true,
+              itemBuilder: (context,index){
+                  return Card(
+                    elevation: 10,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(15),
+                      height: 90,
+                      child: ListTile(
+                        title: Text("Titulo list",
+                          style: TextStyle(
+                          fontSize: 28,
+                          fontFamily: 'titulos',
+                        ),),
+                        subtitle: Text("subtitle"),
+                        leading: Icon(Icons.icecream_outlined,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                  );
+              },
+            )
+          ],
+        ),
+
+
+      ),
     );
   }
 }
